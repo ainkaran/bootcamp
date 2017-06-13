6e8b71522effd9c9db7c437aec1261a272da088d
Ainkaran Pathmanathan <pat.ainkaran@gmail.com>
2017-06-12 19:29:07 -0700

    add new assignments

--- week1/day6/Fibonacci.rb
+++ week1/day6/Fibonacci.rb
@@ -1,29 +1,29 @@
-# print "Enter a number"
-# print "> "
-# number = gets.chomp.to_i
-#
-# # a = Array.new(number, 0)
-#
-# # a = []
-# # a[0] = 1
-# # a[0] = 1
-#
-# newArray = []
-#
-# number.times do |i|
-#     # puts i
-#     if i == 0 || i == 1
-#       newArray << 1
-#
-#     else
-#       newArray << newArray[i-2] + newArray[i-1]
-#     end
-#
-#
-#
-# end
-#
-# puts newArray
+print "Enter a number"
+print "> "
+number = gets.chomp.to_i
+
+# a = Array.new(number, 0)
+
+# a = []
+# a[0] = 1
+# a[0] = 1
+
+newArray = []
+
+number.times do |i|
+    # puts i
+    if i == 0 || i == 1
+      newArray << 1
+
+    else
+      newArray << newArray[i-2] + newArray[i-1]
+    end
+
+
+
+end
+
+puts newArray
 
 
 
@@ -50,4 +50,4 @@ number.times do |i|
 
 end
 
-puts newArray == [1, 1, 2, 3, 6] 
+puts newArray == [1, 1, 2, 3, 6]
